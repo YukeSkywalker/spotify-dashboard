eb04b03eeffa794996970d89ee29cd17f53171dc
Luca <locricchioluca@itis-molinari.eu>
2026-04-19 18:44:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import crypto from "crypto";
-
-dotenv.config();
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const crypto = require('crypto');
 
 const app = express();
+app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("public"));
